ccf7a4ff42f02a8ab6507e14b239fa48161fea77
Avery <trajano.juan@gmail.com>
2022-04-28 14:54:18 -0300

    Create car mocks file

---
+++ src/tests/unit/mocks/car-mocks.ts
@@ -0,0 +1,18 @@
+export const carRequestExample = {
+  model: 'Honda Civic',
+  year: 2019,
+  color: 'red',
+  buyValue: 100000,
+  seatsQty: 4,
+  doorsQty: 4,
+};
+
+export const carResponseExample = {
+  _id: '5e7f8f8f8f8f8f8f8f8f8f8',
+  model: 'Honda Civic',
+  year: 2019,
+  color: 'red',
+  buyValue: 100000,
+  seatsQty: 4,
+  doorsQty: 4,
+};
