5c3b3efc024ec1481b06d9d338b1fa14abac8f66
Avery <trajano.juan@gmail.com>
2022-04-27 23:12:20 -0300

    Update index route

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import BaseController from '../controllers/index';
+
+export default class BaseRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(
+    controller: BaseController<T>,
+    route: string = controller.route,
+  ) {
+    this.router.post(route, controller.create);
+  }
+}
