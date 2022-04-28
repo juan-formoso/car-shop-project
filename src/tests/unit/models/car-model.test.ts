ceb2528c8b3a27dab25e52be3d2ac0ec88fec89f
Avery <trajano.juan@gmail.com>
2022-04-28 15:10:12 -0300

    test: Testa CarModel
    Adiciona testes unitários para CarModel

---
+++ src/tests/unit/models/car-model.test.ts
@@ -0,0 +1,83 @@
+// Escreva testes que cubram, pelo menos, 15% da camada Model.
+// Será validado que cobertura total das linhas dos arquivos na pasta models é maior ou igual a 15%;
+
+import { expect } from 'chai';
+import mongoose from 'mongoose';
+import sinon from 'sinon';
+import CarModel from '../../../models/CarModel';
+import BaseModel from '../../../models/index';
+import { carRequestExample, carResponseExample } from '../mocks/car-mocks';
+
+describe('Testa CarModel', () => {
+  let carModel: CarModel;
+  let stub: sinon.SinonStub;
+
+  before(() => {
+    carModel = new CarModel();
+  });
+
+  afterEach(() => {
+    if (stub) {
+      stub.restore();
+    }
+  });
+
+  describe('Testa o método create', () => {
+    it('deve criar um carro novo', async () => {
+      stub = sinon.stub(BaseModel.prototype, 'create').resolves(carResponseExample);
+
+      const car = await carModel.create(carRequestExample);
+
+      expect(car).to.be.eql(carResponseExample);
+      expect(stub.calledOnce).to.be.true;
+      expect(stub.calledWith(carRequestExample)).to.be.true;
+    });
+  });
+
+  describe('Testa o método read', () => {
+    it('deve retornar todos os carros', async () => {
+      stub = sinon.stub(BaseModel.prototype, 'read').resolves([carResponseExample]);
+
+      const cars = await carModel.read();
+
+      expect(cars).to.be.eql([carResponseExample]);
+      expect(stub.calledOnce).to.be.true;
+    });
+  });
+  
+  describe('Testa o método readOne', () => {
+    it('deve retornar um carro', async () => {
+      stub = sinon.stub(BaseModel.prototype, 'readOne').resolves(carResponseExample);
+
+      const car = await carModel.readOne(carResponseExample._id);
+
+      expect(car).to.be.eql(carResponseExample);
+      expect(stub.calledOnce).to.be.true;
+      expect(stub.calledWith(carResponseExample._id)).to.be.true;
+    });
+  });
+
+  describe('Testa o método update', () => {
+    it('deve atualizar um carro', async () => {
+      stub = sinon.stub(BaseModel.prototype, 'update').resolves(carResponseExample);
+
+      const car = await carModel.update(carResponseExample._id, carRequestExample);
+
+      expect(car).to.be.eql(carResponseExample);
+      expect(stub.calledOnce).to.be.true;
+      expect(stub.calledWith(carResponseExample._id, carRequestExample)).to.be.true;
+    });
+  });
+
+  describe('Testa o método delete', () => {
+    it('deve deletar um carro', async () => {
+      stub = sinon.stub(BaseModel.prototype, 'delete').resolves(carResponseExample);
+
+      const car = await carModel.delete(carResponseExample._id);
+
+      expect(car).to.be.eql(carResponseExample);
+      expect(stub.calledOnce).to.be.true;
+      expect(stub.calledWith(carResponseExample._id)).to.be.true;
+    });
+  });
+});
