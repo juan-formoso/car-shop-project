9d9ed73f62e51f34ebd0f8f0f44de9b2fe62f8f5
Avery <trajano.juan@gmail.com>
2022-04-27 22:08:36 -0300

    Update index controller

---
+++ src/controllers/index.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { ZodIssue } from 'zod';
+import BaseService from '../services/index';
+
+export type ErrorResponse = {
+  error: ZodIssue;
+};
+
+export interface BodyRequest<T> extends Request {
+  body: T;
+}
+
+enum ControllerError {
+  BAD_REQUEST = 'Bad request',
+  NOT_FOUND = 'Object not found',
+  INTERNAL_SERVER_ERROR = 'Internal server error',
+  REQUIRED_ID = 'Required id',
+}
+
+export default abstract class BaseController<T> {
+  protected error = ControllerError;
+
+  abstract route: string;
+
+  constructor(protected service: BaseService<T>) {}
+
+  abstract create(
+    req: BodyRequest<T>, 
+    res: Response<T | ErrorResponse>
+  ): Promise<typeof res>;
+}
